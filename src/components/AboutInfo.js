a5d438600e6f9c527a9002c919fc8de4c756d3fc
Hugh Handcox <82186765+hhandcox@users.noreply.github.com>
2022-08-13 10:44:56 -0500

    Cleaned up Readme

--- src/components/AboutInfo.js
+++ src/components/AboutInfo.js
@@ -14,7 +14,7 @@ const AboutInfo = () => {
                 <h2>a little about me...</h2>
             </div>
             <div className="aboutText mx-auto">
-            Residing in Austin, TX with over 10 years experience producing media for television and higher education. This experience has molded me into a creative problem solver with acute attention to detail - and it's these skills I am bringing with me as I become a Full Stack Developer! Click below to see what knowledge I can bring to the table.
+            Residing in Austin, TX with over 10 years experience producing media for television and higher education. This experience has molded me into a creative problem solver with acute attention to detail - and it's these skills I am bringing with me as I become a Full Stack Developer!
             </div>
         </>
     )
